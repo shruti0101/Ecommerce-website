a96121a3d507066850ab41821b4206c3e9770337
shruti0101 <shrutiguptacu@gmail.com>
2024-10-08 17:43:18 +0530

    second commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -21,6 +21,7 @@ const Navbar = () => {
         <a class="nav-link" href="#">Features</a>
       </li>
       <li class="nav-item">
+        
         <a class="nav-link" href="#">Pricing</a>
       </li>
       <li class="nav-item dropdown">
